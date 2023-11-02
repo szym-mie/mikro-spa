9da552cea5826bcf45e3f4f0404ac5a4eb1d0260
szym-mie <szym.mie@gmail.com>
2023-11-02 17:55:26 +0100

    prevent FOUC

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import PageController from './PageController.js'
 const defaultPageId = 'why-js'
 const getCurrentPageId = () => new URL(window.location).searchParams.get('id') || defaultPageId
 
+/**
+ * await DOM content loaded
+ * @return {Promise<undefined>}
+ */
+const waitDocumentReady = () => ['interactive', 'complete'].includes(document.readyState) ? Promise.resolve() :
+    new Promise((resolve, _reject) => { document.addEventListener('DOMContentLoaded', () => resolve()) })
+
+
 const mainElement = document.querySelector('main')
 const mainPageController = new PageController('pages/', mainElement)
 mainPageController.setPage(getCurrentPageId())
@@ -15,3 +23,8 @@ const createNavLinks = (elems) => {
 }
 
 createNavLinks([...document.querySelectorAll('.nav-to')])
+waitDocumentReady().then(() => { 
+    const preHideElementClassList = document.querySelector('#pre-hide').classList
+    preHideElementClassList.add('pre-hidden')
+    setTimeout(() => { preHideElementClassList.add('pre-hidden-clear') }, 1000)
+})
